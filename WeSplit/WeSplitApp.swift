03cd7dbc8879fdd5ab9a04ca205899af1d28aaaf
sougatoroy3 <sougatoroy007@gmail.com>
2025-07-26 02:19:13 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sougato Roy on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
